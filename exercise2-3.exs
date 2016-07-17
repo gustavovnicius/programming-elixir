4f11e7f6ae66e03fb466b7f0467a207fe92d5f02
Gustavo Vinícius <gustavovnicius@hotmail.com>
2016-07-17 20:48:19 -0300

    First commit

---
+++ exercise2-3.exs
@@ -0,0 +1,20 @@
+fizzbuzzer = fn
+  0, 0, _ -> "fizzbuzz"
+  0, _, _ -> "fizz"
+  _, 0, _ -> "buzz"
+  _, _, c -> c
+end
+
+rem = fn
+  {a, b} -> div(a, b)
+end
+
+fizzbuzz = fn n -> fizzbuzzer.(rem(n, 3), rem(n, 5), n) end
+
+fizzbuzz.(10)
+fizzbuzz.(11)
+fizzbuzz.(12)
+fizzbuzz.(13)
+fizzbuzz.(14)
+fizzbuzz.(15)
+fizzbuzz.(16)
---
+++ exercise4.exs
@@ -0,0 +1,6 @@
+prefix = fn prefix -> (fn suffix -> "#{prefix} #{suffix}" end) end
+
+mrs = prefix.("Mrs")
+mrs.("Smith")
+
+prefix.("Elixir").("Rocks")
